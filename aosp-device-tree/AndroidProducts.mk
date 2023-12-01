1ddebeaf245a70c9434423edd51583039fb92ed1
redznn <h908659@gmail.com>
2023-12-01 01:20:24 +0000

    Add extras for user 13 T1RD33.116-33-13 441420 release-keys
    Signed-off-by: redznn <h908659@gmail.com>

---
+++ aosp-device-tree/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_dubai.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_dubai-user \
+    lineage_dubai-userdebug \
+    lineage_dubai-eng
